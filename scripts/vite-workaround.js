4b6c7302120e611f540596c0c89f455a134d0a69
Luan Pham <pqkluan@gmail.com>
2021-06-08 11:24:01 +0700

    feat: add smui

---
+++ scripts/vite-workaround.js
@@ -0,0 +1,66 @@
+/**
+ * Scrip to fix issue with Svelte Kit source map for error like this:
+ * ENOENT: no such file or directory, open '.../smui-example-sveltekit/node_modules/@material/ripple/index.ts'
+ *
+ * This file was copied from: https://github.com/hperrin/smui-example-sveltekit/blob/master/_helper/vite-workaround.js
+ *
+ * Tracking issues: https://github.com/true-myth/true-myth/issues/130
+ */
+
+import path from 'path';
+import fs from 'fs';
+
+const DEBUG = false;
+
+function log(...params) {
+	if (DEBUG) console.log(...params);
+}
+
+/**
+ *
+ * @param {string} startPath
+ * @param {string} filter
+ * @returns
+ */
+function removeFiles(startPath, filter) {
+	if (!fs.existsSync(startPath)) return log('no dir ', startPath);
+
+	const files = fs.readdirSync(startPath);
+	for (let i = 0; i < files.length; i++) {
+		const filename = path.join(startPath, files[i]);
+		const stat = fs.lstatSync(filename);
+		if (stat.isDirectory()) removeFiles(filename, filter);
+		else if (filename.indexOf(filter) >= 0) {
+			log('-- removing file: ', filename);
+			fs.unlinkSync(filename);
+		}
+	}
+}
+
+/**
+ *
+ * @param {string} startPath
+ * @returns
+ */
+function removeSourceMaps(startPath) {
+	if (!fs.existsSync(startPath)) return log('no dir ', startPath);
+
+	const files = fs.readdirSync(startPath);
+	for (let i = 0; i < files.length; i++) {
+		const filename = path.join(startPath, files[i]);
+		const stat = fs.lstatSync(filename);
+		if (stat.isDirectory()) removeSourceMaps(filename);
+		else if (filename.endsWith('.js')) {
+			const fileContent = fs.readFileSync(filename).toString();
+			const result = fileContent.replace(/^\/\/# sourceMappingURL=.+$/gm, '');
+			if (result !== fileContent) {
+				log('-- removing SourceMap link: ', filename);
+				fs.writeFileSync(filename, result);
+			}
+		}
+	}
+}
+
+console.log('Trigger /scripts/vite-workaround.js');
+removeFiles('node_modules/@material', '.js.map');
+removeSourceMaps('node_modules/@material');
---
+++ src/components/GithubIcon.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import IconButton, { Icon } from '@smui/icon-button';
+	import { mdiGithub } from '@mdi/js';
+	import Svg from '@smui/common/Svg.svelte';
+
+	const href = 'https://github.com/pqkluan/pqkluan.github.io';
+</script>
+
+<IconButton {href} target={'_blank'} aria-label={'Github Icon'}>
+	<Icon component={Svg} viewBox="0 0 24 24">
+		<path fill="currentColor" d={mdiGithub} />
+	</Icon>
+</IconButton>
